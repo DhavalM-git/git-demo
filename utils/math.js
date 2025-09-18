91d712033b7101c63a54c63bca0759bf9b5b11e9
Dhaval.Maheta <dhaval.maheta@eversana.com>
2025-09-18 12:58:22 +0530

    Divide function added

--- utils/math.js
+++ utils/math.js
@@ -11,4 +11,10 @@ function multiply(a, b) {
   return a + b; // should be a * b
 }
 
-module.exports = { add, subtract, multiply };
+// Intentional bug for demo: Division is wrong
+function devide(a, b) {
+  return a - b; // should be a * b
+}
+
+
+module.exports = { add, subtract, multiply, devide };
